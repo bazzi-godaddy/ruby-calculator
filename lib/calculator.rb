d326d464b2d208f63f6e0a34b950c088bad4e11a
Bruno Azzi <bruno-azzi@hotmail.com>
2022-11-16 11:09:15 -0300

    Added Operation class

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,32 +1,7 @@
+require_relative 'calculator/operation'
+
 class Calculator 
-    def calculate(a, b, method)
-        case method
-        when "+"
-            sum(a,b)
-        when "-"
-            subtraction(a, b)
-        when "/"
-            division(a, b)
-        when "*"
-            multiplication(a, b)
-        else
-            nil
-        end
-    end
-
-    def sum(a, b)
-        a + b
-    end
-
-    def subtraction(a, b)
-        a - b
-    end
-
-    def division(a, b)
-        a / b
-    end
-
-    def multiplication(a, b)
-        a * b
+    def calculate(method, *numbers)
+        Calculator::Operation.instance_for(method).call(*numbers)
     end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,28 +7,35 @@ RSpec.describe Calculator do
         context 'when method is sum' do
             it 'returns the sum' do
 
-                expect(calc.calculate(10, 5, "+")).to eq 15
+                expect(calc.calculate(:sum, 10, 5)).to eq 15
             end
         end
 
         context 'when method is substraction' do
             it 'returns the substraction' do
 
-                expect(calc.calculate(25, 5, "-")).to eq 20
+                expect(calc.calculate(:subtraction, 25, 5)).to eq 20
             end
         end
 
         context 'when method is division' do
             it 'returns the division' do
 
-                expect(calc.calculate(10, 2, "/")).to eq 5
+                expect(calc.calculate(:division, 10, 2)).to eq 5
             end
         end
 
         context 'when method is multiplication' do
             it 'returns the multiplication' do
 
-                expect(calc.calculate(10, 5, "*")).to eq 50
+                expect(calc.calculate(:multiplication, 10, 5)).to eq 50
+            end
+        end
+
+        context 'when method is invalid' do
+            it 'returns the exception' do
+
+                expect{ calc.calculate(:invalid, 10, 5) }.to raise_error(ArgumentError)
             end
         end
     end
